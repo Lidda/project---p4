8f8b2c34494b85b00afef5f30515b9829ae2ac14
maikeljonker <43063891+wotwotwotwot@users.noreply.github.com>
2019-05-22 18:24:28 +0200

    Revert "Employee add"
    This reverts commit c185220ce36b418729b9f42939f5e93dc9a2d51d.

--- OrderSystem/OrderSystemUI/OrderSystemUI.cs
+++ OrderSystem/OrderSystemUI/OrderSystemUI.cs
@@ -7,27 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using OrderSystemModel;
-using OrderSystemLogic;
 
-namespace OrderSystemUI
-{
-    public partial class OrderSystemUI : Form
-    {
-        public OrderSystemUI()
-        {
+namespace OrderSystemUI {
+    public partial class OrderSystemUI : Form {
+        public OrderSystemUI() {
             InitializeComponent();
-        }
-
-        private void BTN_AddEmployee_Click(object sender, EventArgs e)
-        {
-            EmployeeLogic employeeLogic = new EmployeeLogic();
-            Employee employee = new Employee();
-            employee.name = TXTB_AddEmployeeName.Text;
-            employee.username = TXTB_AddUsername.Text;
-            employee.password = TXTB_AddPassword.Text;
-            employee.type = (OrderSystemModel.Type)Enum.Parse(typeof(OrderSystemModel.Type), DB_AddEmployeeType.SelectedItem.ToString());
-            employeeLogic.AddEmployee(employee);
         }
     }
 }
