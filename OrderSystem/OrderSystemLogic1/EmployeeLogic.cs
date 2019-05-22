c185220ce36b418729b9f42939f5e93dc9a2d51d
maikeljonker <43063891+wotwotwotwot@users.noreply.github.com>
2019-05-22 18:12:13 +0200

    Employee add

---
+++ OrderSystem/OrderSystemLogic1/EmployeeLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderSystemDAL;
+using OrderSystemModel;
+
+namespace OrderSystemLogic
+{
+    public class EmployeeLogic
+    {
+        EmployeeDAL employee_db = new EmployeeDAL();
+
+        public void AddEmployee(Employee employee) {
+            try
+            {
+                employee_db.AddEmployee(employee);
+            }
+            catch
+            {
+                throw new Exception("Yikes, dat is niet best");
+            }
+        }
+    }
+}
