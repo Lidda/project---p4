0672a1aedceaf5a5649c98c64ef0a913bad2a418
Ja man <zaka_osm@hotmail.com>
2019-05-31 16:19:26 +0200

    Bar en kitchen begin gemaakt

--- OrderSystem/OrderSystemUI/MainUI/KitchenUI.Designer.cs
+++ OrderSystem/OrderSystemUI/MainUI/KitchenUI.Designer.cs
@@ -24,6 +24,21 @@
         /// </summary>
         private void InitializeComponent() {
             this.btnLogout = new System.Windows.Forms.Button();
+            this.txt_sortOldest = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.listView5 = new System.Windows.Forms.ListView();
+            this.listView6 = new System.Windows.Forms.ListView();
+            this.Refresh_btn = new System.Windows.Forms.Button();
+            this.btn_markReady1 = new System.Windows.Forms.Button();
+            this.btn_markReady2 = new System.Windows.Forms.Button();
+            this.btn_markReady3 = new System.Windows.Forms.Button();
+            this.btn_markReady4 = new System.Windows.Forms.Button();
+            this.btn_markReady5 = new System.Windows.Forms.Button();
+            this.btn_markReady6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLogout
@@ -31,33 +46,220 @@
             this.btnLogout.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnLogout.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogout.Font = new System.Drawing.Font("Roboto", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLogout.Location = new System.Drawing.Point(19, 18);
-            this.btnLogout.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnLogout.Location = new System.Drawing.Point(6, 6);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(190, 66);
+            this.btnLogout.Size = new System.Drawing.Size(60, 23);
             this.btnLogout.TabIndex = 3;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // txt_sortOldest
+            // 
+            this.txt_sortOldest.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_sortOldest.Location = new System.Drawing.Point(364, 57);
+            this.txt_sortOldest.Name = "txt_sortOldest";
+            this.txt_sortOldest.Size = new System.Drawing.Size(100, 13);
+            this.txt_sortOldest.TabIndex = 4;
+            this.txt_sortOldest.Text = "Sort by oldest";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(189, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 13);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Kitchen Overview";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.Tomato;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(30, 127);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(114, 197);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.VirtualMode = true;
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.Color.Tomato;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(189, 127);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(114, 197);
+            this.listView2.TabIndex = 7;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.VirtualMode = true;
+            // 
+            // listView3
+            // 
+            this.listView3.BackColor = System.Drawing.Color.Tomato;
+            this.listView3.GridLines = true;
+            this.listView3.Location = new System.Drawing.Point(350, 127);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(114, 197);
+            this.listView3.TabIndex = 8;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            this.listView3.VirtualMode = true;
+            // 
+            // listView4
+            // 
+            this.listView4.BackColor = System.Drawing.Color.Tomato;
+            this.listView4.GridLines = true;
+            this.listView4.Location = new System.Drawing.Point(30, 404);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(114, 197);
+            this.listView4.TabIndex = 9;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            this.listView4.View = System.Windows.Forms.View.Details;
+            this.listView4.VirtualMode = true;
+            // 
+            // listView5
+            // 
+            this.listView5.BackColor = System.Drawing.Color.Tomato;
+            this.listView5.GridLines = true;
+            this.listView5.Location = new System.Drawing.Point(189, 404);
+            this.listView5.Name = "listView5";
+            this.listView5.Size = new System.Drawing.Size(114, 197);
+            this.listView5.TabIndex = 10;
+            this.listView5.UseCompatibleStateImageBehavior = false;
+            this.listView5.View = System.Windows.Forms.View.Details;
+            this.listView5.VirtualMode = true;
+            // 
+            // listView6
+            // 
+            this.listView6.BackColor = System.Drawing.Color.Tomato;
+            this.listView6.GridLines = true;
+            this.listView6.Location = new System.Drawing.Point(350, 404);
+            this.listView6.Name = "listView6";
+            this.listView6.Size = new System.Drawing.Size(114, 197);
+            this.listView6.TabIndex = 11;
+            this.listView6.UseCompatibleStateImageBehavior = false;
+            this.listView6.View = System.Windows.Forms.View.Details;
+            this.listView6.VirtualMode = true;
+            // 
+            // Refresh_btn
+            // 
+            this.Refresh_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Refresh_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Refresh_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Refresh_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Refresh_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Refresh_btn.Location = new System.Drawing.Point(211, 679);
+            this.Refresh_btn.Name = "Refresh_btn";
+            this.Refresh_btn.Size = new System.Drawing.Size(60, 23);
+            this.Refresh_btn.TabIndex = 12;
+            this.Refresh_btn.Text = "Refresh";
+            this.Refresh_btn.UseVisualStyleBackColor = false;
+            // 
+            // btn_markReady1
+            // 
+            this.btn_markReady1.Location = new System.Drawing.Point(39, 343);
+            this.btn_markReady1.Name = "btn_markReady1";
+            this.btn_markReady1.Size = new System.Drawing.Size(94, 23);
+            this.btn_markReady1.TabIndex = 13;
+            this.btn_markReady1.Text = "Mark as ready";
+            this.btn_markReady1.UseVisualStyleBackColor = true;
+            // 
+            // btn_markReady2
+            // 
+            this.btn_markReady2.Location = new System.Drawing.Point(195, 343);
+            this.btn_markReady2.Name = "btn_markReady2";
+            this.btn_markReady2.Size = new System.Drawing.Size(94, 23);
+            this.btn_markReady2.TabIndex = 14;
+            this.btn_markReady2.Text = "Mark as ready";
+            this.btn_markReady2.UseVisualStyleBackColor = true;
+            // 
+            // btn_markReady3
+            // 
+            this.btn_markReady3.Location = new System.Drawing.Point(364, 343);
+            this.btn_markReady3.Name = "btn_markReady3";
+            this.btn_markReady3.Size = new System.Drawing.Size(94, 23);
+            this.btn_markReady3.TabIndex = 15;
+            this.btn_markReady3.Text = "Mark as ready";
+            this.btn_markReady3.UseVisualStyleBackColor = true;
+            // 
+            // btn_markReady4
+            // 
+            this.btn_markReady4.Location = new System.Drawing.Point(39, 620);
+            this.btn_markReady4.Name = "btn_markReady4";
+            this.btn_markReady4.Size = new System.Drawing.Size(94, 23);
+            this.btn_markReady4.TabIndex = 16;
+            this.btn_markReady4.Text = "Mark as ready";
+            this.btn_markReady4.UseVisualStyleBackColor = true;
+            // 
+            // btn_markReady5
+            // 
+            this.btn_markReady5.Location = new System.Drawing.Point(195, 620);
+            this.btn_markReady5.Name = "btn_markReady5";
+            this.btn_markReady5.Size = new System.Drawing.Size(94, 23);
+            this.btn_markReady5.TabIndex = 17;
+            this.btn_markReady5.Text = "Mark as ready";
+            this.btn_markReady5.UseVisualStyleBackColor = true;
+            // 
+            // btn_markReady6
+            // 
+            this.btn_markReady6.Location = new System.Drawing.Point(364, 620);
+            this.btn_markReady6.Name = "btn_markReady6";
+            this.btn_markReady6.Size = new System.Drawing.Size(94, 23);
+            this.btn_markReady6.TabIndex = 18;
+            this.btn_markReady6.Text = "Mark as ready";
+            this.btn_markReady6.UseVisualStyleBackColor = true;
+            // 
             // KitchenUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1469, 1938);
+            this.ClientSize = new System.Drawing.Size(501, 741);
+            this.Controls.Add(this.btn_markReady6);
+            this.Controls.Add(this.btn_markReady5);
+            this.Controls.Add(this.btn_markReady4);
+            this.Controls.Add(this.btn_markReady3);
+            this.Controls.Add(this.btn_markReady2);
+            this.Controls.Add(this.btn_markReady1);
+            this.Controls.Add(this.Refresh_btn);
+            this.Controls.Add(this.listView6);
+            this.Controls.Add(this.listView5);
+            this.Controls.Add(this.listView4);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_sortOldest);
             this.Controls.Add(this.btnLogout);
-            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "KitchenUI";
             this.Text = "KitchenUI";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.TextBox txt_sortOldest;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.ListView listView5;
+        private System.Windows.Forms.ListView listView6;
+        private System.Windows.Forms.Button Refresh_btn;
+        private System.Windows.Forms.Button btn_markReady1;
+        private System.Windows.Forms.Button btn_markReady2;
+        private System.Windows.Forms.Button btn_markReady3;
+        private System.Windows.Forms.Button btn_markReady4;
+        private System.Windows.Forms.Button btn_markReady5;
+        private System.Windows.Forms.Button btn_markReady6;
     }
 }
